ffbaa1d4b0c38a4ae3a44b75712476a2a01c4ae9
James Snyder <james@jrsnyder.tech>
2023-03-18 22:24:28 -0400

    hard-coded character dropdown

---
+++ get_charlist.py
@@ -0,0 +1,10 @@
+from flask_mysqldb import MySQL
+def get_charlist(mysql):
+    charlist = []
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT characterName FROM devttg.war_characters;')
+    rv = cur.fetchall()
+    for i in rv:
+        charlist.append(i[0])
+    cur.close()
+    return charlist
